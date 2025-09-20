047fc0b1a394689ee7c5494e959df23e8d6c628f
sony <sony@LAPTOP-5H8BE6JH>
2025-09-20 20:55:58 +0530

    commit and push

---
+++ TNS.first/src/Interfaces/Main.java
@@ -0,0 +1,10 @@
+package Interfaces;
+
+public class Main {
+    public static void main(String[] args) {
+        Testclass obj = new Testclass();
+        obj.methodA(); 
+        obj.methodB(); 
+    }
+}
+
---
+++ TNS.first/src/Interfaces/Testclass.java
@@ -0,0 +1,10 @@
+package Interfaces;
+class Testclass implements Interface2 {
+    public void methodA() {
+        System.out.println("Method A implemented.");
+    }
+    public void methodB() {
+        System.out.println("Method B implemented.");
+    }
+}
+
