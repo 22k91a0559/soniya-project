f0c48d50e2b2eba739ab971da426c030c1a46a68
sony <sony@LAPTOP-5H8BE6JH>
2025-10-09 23:29:06 +0530

    commit

---
+++ TNSIF(New)/src/collections/Priorityqueue.java
@@ -0,0 +1,24 @@
+
+package collections;
+import java.util.PriorityQueue;
+
+
+public class Priorityqueue {
+
+	public static void main(String[] args) {
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        pq.add(50);
+        pq.add(20);
+        pq.add(40);
+        pq.add(10);
+
+        System.out.println("PriorityQueue: " + pq);
+
+        System.out.println("Removed element (poll): " + pq.poll());
+
+        System.out.println("Peek element: " + pq.peek());
+
+	}
+
+}
